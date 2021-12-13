76a08b14f0ec58585bf10ceff0a2377bdd7d93fe
Isabela Savastano <srtaisabela@gmail.com>
2021-12-13 13:33:59 +0100

    inicial page

--- client/src/start.js
+++ client/src/start.js
@@ -1,7 +1,28 @@
 import ReactDOM from "react-dom";
+import { useState, useEffect } from "react";
 
 ReactDOM.render(<HelloWorld />, document.querySelector("main"));
 
 function HelloWorld() {
-    return <div>Hello, World!</div>;
+    const [ask, setAsk] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setAsk(true);
+        }, 8000);
+    }, []);
+
+    return (
+        <>
+            <div className="ripple-background">
+                <div className="circle xxlarge shade1"></div>
+                <div className="circle xlarge shade2"></div>
+                <div className="circle large shade3"></div>
+                <div className="circle medium shade4"></div>
+                <div className="circle smal shade5"></div>
+                <p className="breathe">TAKE A DEEP BREATH</p>
+            </div>
+            {ask && <button>Ask your question</button>}
+        </>
+    );
 }
