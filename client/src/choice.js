740cd80fa25084786245fff7a5281f3816daa457
Isabela Savastano <srtaisabela@gmail.com>
2021-12-15 14:52:07 +0100

    added text-generator modal

---
+++ client/src/choice.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Choice() {
+    return (
+        <div className="stage2-container">
+            <button className="choice">Oracle Wisdom</button>
+            <button className="choice">Ask a question</button>
+        </div>
+    );
+}
--- client/src/start.js
+++ client/src/start.js
@@ -1,10 +1,11 @@
 import ReactDOM from "react-dom";
 import { useState, useEffect } from "react";
 
-ReactDOM.render(<HelloWorld />, document.querySelector("main"));
+ReactDOM.render(<FortuneTeller />, document.querySelector("main"));
 
-function HelloWorld() {
+function FortuneTeller() {
     const [ask, setAsk] = useState(false);
+    const [stage, setStage] = useState(1);
 
     useEffect(() => {
         setTimeout(() => {
@@ -12,17 +13,37 @@ function HelloWorld() {
         }, 8000);
     }, []);
 
-    return (
-        <>
-            <div className="ripple-background">
-                <div className="circle xxlarge shade1"></div>
-                <div className="circle xlarge shade2"></div>
-                <div className="circle large shade3"></div>
-                <div className="circle medium shade4"></div>
-                <div className="circle smal shade5"></div>
-                <p className="breathe">TAKE A DEEP BREATH</p>
-            </div>
-            {ask && <button>Ask your question</button>}
-        </>
-    );
+    const showStage = () => {
+        if (stage === 1) {
+            return (
+                <>
+                    <div className="ripple-background">
+                        <div className="circle xxlarge shade1"></div>
+                        <div className="circle xlarge shade2"></div>
+                        <div className="circle large shade3"></div>
+                        <div className="circle medium shade4"></div>
+                        <div className="circle small shade5"></div>
+                        <p className="breathe">TAKE A DEEP BREATH</p>
+                    </div>
+
+                    {ask && (
+                        <button className="start" onClick={() => setStage(2)}>
+                            START
+                        </button>
+                    )}
+                </>
+            );
+        } else if (stage === 2) {
+            return (
+                <div className="stage2-container">
+                    <button className="choice">Oracle Wisdom</button>
+                    <button className="choice">Ask a question</button>
+                </div>
+            );
+        } else if (this.state.stage === 3) {
+            return <></>;
+        }
+    };
+
+    return <>{showStage()}</>;
 }
